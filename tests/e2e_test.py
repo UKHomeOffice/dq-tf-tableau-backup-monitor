3582fe5c9eb952e3a13c3fc6d995bec5db332e77
Ian Relf <ian.relf@digital.homeoffice.gov.uk>
2023-12-22 17:36:56 +0000

    Migrate TF 0.12 -> 0.13  - inc versions.tf  - remove unsupported test

--- tests/e2e_test.py
+++ tests/e2e_test.py
@@ -13,7 +13,6 @@ class TestE2E(unittest.TestCase):
             provider "aws" {
               region = "eu-west-2"
               skip_credentials_validation = true
-              skip_get_ec2_platforms = true
             }
 
             module "tableau_backup_monitor" {
